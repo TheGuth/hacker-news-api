d2bae8ed3eb94337f4bd4b510b974e4453af94e9
James Guthrie <jamesrguthrie@gmail.com>
2017-02-27 16:12:59 -0500

    added mlab databases to config file

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                             global.TEST_DATABASE_URL ||
-                            'mongodb://localhost/test-hn-api';
+                            'mongodb://me:pass@ds163718.mlab.com:63718/testhackerapi';
 
 exports.PORT = process.env.PORT || 8080;
