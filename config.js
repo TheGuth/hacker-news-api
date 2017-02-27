25f61d1228dea9cfe5192010376c037d1e66a369
James Guthrie <jamesrguthrie@gmail.com>
2017-02-27 16:01:33 -0500

    added travis yml file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                      global.DATABASE_URL ||
-                     'mongodb://localhost/hn-api';
+                     'mongodb://me:pass@ds163679.mlab.com:63679/hackerapi';
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                             global.TEST_DATABASE_URL ||
